7505500ee2d5672d9a7a8093deed8a4ba3a75ed8
tinvukhac <tinvukhac@gmail.com>
2018-05-21 10:55:24 -0700

    update GeoTweet class

--- src/main/java/edu/umn/cs/spatialHadoop/twitter/GeoTweet.java
+++ src/main/java/edu/umn/cs/spatialHadoop/twitter/GeoTweet.java
@@ -8,8 +8,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.wololo.jts2geojson.GeoJSONReader;
-
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -18,6 +16,7 @@ import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.geojson.GeoJsonReader;
 
 import edu.umn.cs.spatialHadoop.core.OGCJTSShape;
+import edu.umn.cs.spatialHadoop.core.Shape;
 import edu.umn.cs.spatialHadoop.io.TextSerializable;
 
 public class GeoTweet extends OGCJTSShape implements Writable, TextSerializable {
@@ -26,7 +25,7 @@ public class GeoTweet extends OGCJTSShape implements Writable, TextSerializable 
 
 	public GeoTweet() {
 		// TODO Auto-generated constructor stub
-//		System.out.println("Init GeoTweet");
+		// System.out.println("Init GeoTweet");
 	}
 
 	public GeoTweet(Geometry geom) {
@@ -34,9 +33,15 @@ public class GeoTweet extends OGCJTSShape implements Writable, TextSerializable 
 		// TODO Auto-generated constructor stub
 	}
 
+	public GeoTweet(String json) {
+		this.json = json;
+		this.geom = parseGeometryFromJsonString(json);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public Text toText(Text text) {
-//		System.out.println("toText");
+		// System.out.println("toText");
 		byte[] temp = (json == null ? "" : json).getBytes();
 		text.append(temp, 0, temp.length);
 		return text;
@@ -46,44 +51,96 @@ public class GeoTweet extends OGCJTSShape implements Writable, TextSerializable 
 	public void fromText(Text text) {
 		// System.out.println("fromText");
 		this.json = text.toString();
-//		this.json = "{ \"type\": \"FeatureCollection\", \"features\": [ { \"type\": \"Feature\", \"properties\": { \"name\": \"Van Dorn Street\", \"marker-color\": \"#0000ff\", \"marker-symbol\": \"rail-metro\", \"line\": \"blue\" }, \"geometry\": { \"type\": \"Point\", \"coordinates\": [ -77.12911152370515, 38.79930767201779 ] } }, { \"type\": \"Feature\", \"properties\": { \"name\": \"Franconia-Springfield\", \"marker-color\": \"#0000ff\", \"marker-symbol\": \"rail-metro\", \"line\": \"blue\" }, \"geometry\": { \"type\": \"Point\", \"coordinates\": [ -77.16797018042666, 38.766521892689916 ] } }] }";
-//		this.json = "{\"geometry\": { \"type\": \"Point\", \"coordinates\": [ -77.12911152370515, 38.79930767201779 ] }}";
-//		this.json = "{ \"type\": \"Point\", \"coordinates\": [ -77.12911152370515, 38.79930767201779 ] }";
-		JSONObject tweet = new JSONObject(json);
-		try {
-			JSONObject bbox = tweet.getJSONObject("place").getJSONObject("bounding_box");
-			GeoJsonReader reader = new GeoJsonReader();
-			try {
-				this.geom = reader.read(bbox.toString());
-//				this.geom = reader.read("{\"coordinates\":[[[-80.282775,26.089889],[-80.282775,26.105196],[-80.265788,26.105196],[-80.265788,26.089889],[-80.282775,26.089889]]],\"type\":\"Polygon\"}");
-			} 
-			catch (ParseException e2) {
-				// TODO Auto-generated catch block
-				this.geom = new GeometryFactory().createPoint(new Coordinate(0, 0));
-				System.out.println("ParseException");
-//				System.out.println(json);
-				System.out.println(bbox.toString());
-				 e2.printStackTrace();
-			}
-		} catch (JSONException e1) {
-//			System.out.println("JSONException");
-//			System.out.println(json);
-			this.geom = new GeometryFactory().createPoint(new Coordinate(0, 0));
-			 e1.printStackTrace();
-		}
+		this.geom = parseGeometryFromJsonString(json);
+		// this.json = "{ \"type\": \"FeatureCollection\", \"features\": [ { \"type\":
+		// \"Feature\", \"properties\": { \"name\": \"Van Dorn Street\",
+		// \"marker-color\": \"#0000ff\", \"marker-symbol\": \"rail-metro\", \"line\":
+		// \"blue\" }, \"geometry\": { \"type\": \"Point\", \"coordinates\": [
+		// -77.12911152370515, 38.79930767201779 ] } }, { \"type\": \"Feature\",
+		// \"properties\": { \"name\": \"Franconia-Springfield\", \"marker-color\":
+		// \"#0000ff\", \"marker-symbol\": \"rail-metro\", \"line\": \"blue\" },
+		// \"geometry\": { \"type\": \"Point\", \"coordinates\": [ -77.16797018042666,
+		// 38.766521892689916 ] } }] }";
+		// this.json = "{\"geometry\": { \"type\": \"Point\", \"coordinates\": [
+		// -77.12911152370515, 38.79930767201779 ] }}";
+		// this.json = "{ \"type\": \"Point\", \"coordinates\": [ -77.12911152370515,
+		// 38.79930767201779 ] }";
+//		JSONObject tweet = new JSONObject(json);
+//		try {
+//			JSONObject bbox = tweet.getJSONObject("place").getJSONObject("bounding_box");
+//			GeoJsonReader reader = new GeoJsonReader();
+//			try {
+//				this.geom = reader.read(bbox.toString());
+//				// this.geom =
+//				// reader.read("{\"coordinates\":[[[-80.282775,26.089889],[-80.282775,26.105196],[-80.265788,26.105196],[-80.265788,26.089889],[-80.282775,26.089889]]],\"type\":\"Polygon\"}");
+//			} catch (ParseException e2) {
+//				// TODO Auto-generated catch block
+//				this.geom = new GeometryFactory().createPoint(new Coordinate(0, 0));
+//				System.out.println("ParseException");
+//				// System.out.println(json);
+//				System.out.println(bbox.toString());
+//				e2.printStackTrace();
+//			}
+//		} catch (JSONException e1) {
+//			// System.out.println("JSONException");
+//			// System.out.println(json);
+//			this.geom = new GeometryFactory().createPoint(new Coordinate(0, 0));
+//			e1.printStackTrace();
+//		}
 	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
-//		System.out.println("public void write");
+		// System.out.println("public void write");
 		super.write(out);
 		out.writeUTF(this.json);
 	}
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
-//		System.out.println("public void readFields");
+		// System.out.println("public void readFields");
 		super.readFields(in);
 		this.json = in.readUTF();
+	}
+
+	@Override
+	public Shape clone() {
+		GeoTweet copy = new GeoTweet(this.json);
+		return copy;
+	}
+	
+	private Geometry parseGeometryFromJsonString(String json) {
+		
+		if(json != null) {
+			Geometry geom;
+			
+			JSONObject tweet = new JSONObject(json);
+			try {
+				JSONObject bbox = tweet.getJSONObject("place").getJSONObject("bounding_box");
+				GeoJsonReader reader = new GeoJsonReader();
+				try {
+					geom = reader.read(bbox.toString());
+					// this.geom =
+					// reader.read("{\"coordinates\":[[[-80.282775,26.089889],[-80.282775,26.105196],[-80.265788,26.105196],[-80.265788,26.089889],[-80.282775,26.089889]]],\"type\":\"Polygon\"}");
+				} catch (ParseException e2) {
+					// TODO Auto-generated catch block
+					geom = new GeometryFactory().createPoint(new Coordinate(0, 0));
+					System.out.println("ParseException");
+					// System.out.println(json);
+					System.out.println(bbox.toString());
+					e2.printStackTrace();
+				}
+			} catch (JSONException e1) {
+				 System.out.println("JSONException");
+				// System.out.println(json);
+				geom = new GeometryFactory().createPoint(new Coordinate(0, 0));
+				e1.printStackTrace();
+			}
+			
+			return geom;
+		} else {
+//			System.out.println("Json input is null");
+			return null;
+		}
 	}
 }
